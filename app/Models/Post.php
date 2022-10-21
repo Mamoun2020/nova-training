8b5a854755497dbc684c127b799c3aaec3f33d84
Mamoun Abu Salah <70561744+Mamoun2020@users.noreply.github.com>
2022-10-21 21:34:53 +0300

    action PublishPost add it in model to show every action in the Post Details page

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,15 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Nova\Actions\Actionable;
 
 
 class Post extends Model
 {
     use HasFactory;
-
+    use Actionable;
  protected $casts =[
 'publish_at'=> 'datetime',
 'publish_until'=> 'datetime',
+ ];
+ protected $fillable = [
+   'is_published',
  ];
  public function user(){
      return $this->belongsTo(User::Class);
